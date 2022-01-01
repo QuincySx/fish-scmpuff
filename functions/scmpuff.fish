314e4d6ab91c74321593890d322e0cf09a836f4e
QuincySx <QuincySxx@protonmail.com>
2022-01-01 20:51:58 +0800

    Update scmpuff.fish

--- functions/scmpuff.fish
+++ functions/scmpuff.fish
@@ -1,21 +1,22 @@
 # Remove any existing git alias or function
 unalias git > /dev/null 2>&1
-unset -f git > /dev/null 2>&1
+functions -e git > /dev/null 2>&1
 
-# Use the full path to git to avoid infinite loop with git function
-SCMPUFF_GIT_CMD=${SCMPUFF_GIT_CMD:-"$(\which git)"}
-export SCMPUFF_GIT_CMD
+# User the full path to git to avoid infinite loop with git function
+set -x SCMPUFF_GIT_CMD (which git)
 
-function git() {
-  case $1 in
-    commit|blame|log|rebase|merge)
-      scmpuff exec -- "$SCMPUFF_GIT_CMD" "$@";;
-    checkout|diff|rm|reset|restore)
-      scmpuff exec --relative -- "$SCMPUFF_GIT_CMD" "$@";;
-    add)
-      scmpuff exec -- "$SCMPUFF_GIT_CMD" "$@"
-      scmpuff_status;;
-    *)
-      "$SCMPUFF_GIT_CMD" "$@";;
-  esac
-}
+# Wrap git with the 'hub' github wrapper, if installed
+if type hub > /dev/null 2>&1; set -x SCMPUFF_GIT_CMD "hub"; end
+
+function git
+  switch $1
+  case commit blame log rebase merge
+    eval "scmpuff expand --"$SCMPUFF_GIT_CMD" "$@
+  case checkout diff rm reset
+    eval "scmpuff expand --relative -- "$SCMPUFF_GIT_CMD" "$@
+  case add
+    eval "scmpuff expand -- "$SCMPUFF_GIT_CMD" "$@
+    scmpuff_status
+  case '*'
+    eval $SCMPUFF_GIT_CMD $@
+end
